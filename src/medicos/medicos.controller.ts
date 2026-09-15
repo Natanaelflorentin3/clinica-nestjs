2c543d02a76d247c92a7cdbabc442ddb269d26ed
natanael <natanaelflorentin3@gmail.com>
2026-09-15 14:17:50 -0300

    feat: CRUD completo de pacientes y medicos con decoradores de NestJS

--- src/medicos/medicos.controller.ts
+++ src/medicos/medicos.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Post, Put, Delete, Param, Body, NotFoundException } from '@nestjs/common'
 import { MedicosService } from './medicos.service'
 
 @Controller('medicos')
@@ -8,5 +8,29 @@ export class MedicosController {
   @Get()
   findAll() {
     return this.medicosService.findAll()
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const medico = await this.medicosService.findOne(Number(id))
+    if (!medico) {
+      throw new NotFoundException(`No se encontró un médico con id ${id}.`)
+    }
+    return medico
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return this.medicosService.create(body)
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return this.medicosService.update(Number(id), body)
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.medicosService.remove(Number(id))
   }
 }
