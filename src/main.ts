ce5452eaf011e45a092187c4f67fddd139388869
natanael <natanaelflorentin3@gmail.com>
2026-09-22 18:36:48 -0300

    docs: documentacion completa de la API con Swagger

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module';
 import { ValidationPipe } from '@nestjs/common'
 import { PrismaExceptionFilter } from './prisma/prisma-exception.filter';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 async function bootstrap() {
+  const config = new DocumentBuilder()
+  .setTitle('Clínica Salud Integral')
+  .setDescription('API de la clínica, migrada a NestJS')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build()
+  
+  
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
   app.useGlobalFilters(new PrismaExceptionFilter())
+  const document = SwaggerModule.createDocument(app, config)
+SwaggerModule.setup('api/docs', app, document)
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
